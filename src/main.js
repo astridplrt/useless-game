a949256fe3b616456d0de1609ff6b7b14791cf1b
Astrid Polart <astrid.polart@gmail.com>
2025-01-23 21:11:09 +0100

    Set and style base

--- src/main.js
+++ src/main.js
@@ -6,14 +6,26 @@ const pagePosition = document.getElementById("pagePosition");
 const screenPosition = document.getElementById("screenPosition");
 const movementPosition = document.getElementById("movementPosition");
 
-let textClientPosition = "clientX/clientY : ";
-let textOffsetPosition = "offsetX/offsetY : ";
-let textPagePosition = "pageX/pageY : ";
-let textScreenPosition = "screenX/screenY : ";
-let textMovementPosition = "movementX/movementY : ";
+let textClient = "clientX/clientY : ";
+let textOffset = "offsetX/offsetY : ";
+let textPage = "pageX/pageY : ";
+let textScreen = "screenX/screenY : ";
+let textMovement = "movementX/movementY : ";
 
-clientPosition.innerText = textClientPosition;
-offsetPosition.innerText = textOffsetPosition;
-pagePosition.innerText = textPagePosition;
-screenPosition.innerText = textScreenPosition;
-movementPosition.innerText = textMovementPosition;
+let textClientExplain = " dans les coordonnées locales (contenu du DOM)";
+let textOffsetExplain = " par rapport à cet encadré (sur lequel on écoute l'event)";
+let textPageExplain = " par rapport au document entier";
+let textScreenExplain = " par rapport à à la fenêtre du navigateur";
+let textMovementExplain = " par rapport à sa position lors du dernier event mousemove";
+
+clientPosition.innerText = textClient;
+offsetPosition.innerText = textOffset;
+pagePosition.innerText = textPage;
+screenPosition.innerText = textScreen;
+movementPosition.innerText = textMovement;
+
+clientPosition.setAttribute("title", textClientExplain)
+offsetPosition.setAttribute("title", textOffsetExplain)
+pagePosition.setAttribute("title", textPageExplain)
+screenPosition.setAttribute("title", textScreenExplain)
+movementPosition.setAttribute("title", textMovementExplain)
